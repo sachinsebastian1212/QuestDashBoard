f5e05b35d068ef5838b989bba23810e2b6e548de
sachinsebastian12 <sachinsebastian12@gmail.com>
2019-02-25 20:34:24 +0530

    implementing tree visualisation from json

---
+++ test_data.js
@@ -0,0 +1,42 @@
+var data={
+    "id": "qs_1",
+    "type": "questSet",
+    "children": [
+        {
+            "id": "q_1",
+            "type": "quest",
+            "children": [
+                {
+                    "id": "ol_1",
+                    "type": "objectiveList",
+                    "children": [
+                        {
+                            "id": "o_1",
+                            "type": "objective"
+                        },
+                        {
+                            "id": "o_2",
+                            "type": "objective"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id": "q_2",
+            "type": "quest",
+            "children": [
+                {
+                    "id": "ol_2",
+                    "type": "objectiveList",
+                    "children": [
+                        {
+                            "id": "o_3",
+                            "type": "objective"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
