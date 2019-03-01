fc7632e34421ab13244971688a7973ee109296fc
sachinsebastian12 <sachinsebastian12@gmail.com>
2019-03-01 11:28:34 +0530

    Changed files to dynamically create nodes for tree in view.html using jquery

--- test_data.js
+++ test_data.js
@@ -1,42 +1,58 @@
-var data={
-    "id": "qs_1",
-    "type": "questSet",
+var data = [{
+    "data": {
+        "id": "qs_1",
+        "type": "questSet"
+    },
     "children": [
         {
-            "id": "q_1",
-            "type": "quest",
+            "data": {
+                "id": "q_1",
+                "type": "quest"
+            },
             "children": [
                 {
-                    "id": "ol_1",
-                    "type": "objectiveList",
+                    "data": {
+                        "id": "ol_1",
+                        "type": "objectiveList"
+                    },
                     "children": [
                         {
-                            "id": "o_1",
-                            "type": "objective"
+                            "data": {
+                                "id": "o_1",
+                                "type": "objective"
+                            }
                         },
                         {
-                            "id": "o_2",
-                            "type": "objective"
+                            "data": {
+                                "id": "o_2",
+                                "type": "objective"
+                            }
                         }
                     ]
                 }
             ]
         },
         {
-            "id": "q_2",
-            "type": "quest",
+            "data": {
+                "id": "q_2",
+                "type": "quest"
+            },
             "children": [
                 {
-                    "id": "ol_2",
-                    "type": "objectiveList",
+                    "data": {
+                        "id": "ol_2",
+                        "type": "objectiveList",
+                    },
                     "children": [
                         {
-                            "id": "o_3",
-                            "type": "objective"
+                            "data": {
+                                "id": "o_3",
+                                "type": "objective"
+                            }
                         }
                     ]
                 }
             ]
         }
     ]
-}
+}];
