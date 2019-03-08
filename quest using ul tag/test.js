2e16e20421576742a706311e261a783c83cdd27d
sachinsebastian12 <sachinsebastian12@hotmail.com>
2019-03-08 11:11:49 +0530

    Added list creation using js object data

--- quest using ul tag/test.js
+++ quest using ul tag/test.js
@@ -1,4 +1,4 @@
-var html_ids = { qs: 0, q: 0, ol: 0, o: 0 };
+var ids_html = { o_id: 0, ol_id: 0, q_id: 0, qs_id: 0 };
 
 
 
@@ -25,22 +25,98 @@ function setDefaultChallangeTitle() {
 //     $('#oList').last().attr("selected","selected");
 // }
 
+function traverse(data) {
+    if (data !== null && data.constructor === Array) {
+        for (var index in data) {
+
+            for (var keys in data[index]) {
+                if (keys == "children")
+                    traverse(data[index][keys]);
+                else if (keys == "data") {
+                    createNode(data[index][keys]);
+                }
+            }
+        }
+    }
+}
+function createNode(data) {
+
+    var list;
+    var list_item;
+
+    switch (data.type) {
+        case "objective":
+            ids_html.o_id++;
+            list = "#list4" + ids_html.qs_id;
+            list_item = "<li id = 'o_'" + ids_html.o_id + "></li>";
+            $(list).append(list_item);
+
+            data.html_id = "o_" + ids_html.o_id;
+            break;
+        case "objectiveList":
+            ids_html.ol_id++;
+            list = "#list3" + ids_html.qs_id;
+            list_item = "<li id = 'ol_'" + ids_html.ol_id + "></li>";
+            $(list).before(list_item);
+
+            data.html_id = "ol_" + ids_html.ol_id;
+            break;
+        case "quest":
+            ids_html.q_id++;
+            list = "#list3" + ids_html.qs_id;
+            list_item = "<li id = 'q_'" + ids_html.q_id + "></li>";
+            $(list).before(list_item);
+
+            data.html_id = "q_" + ids_html.q_id;
+            break;
+        case "questSet":
+            ids_html.qs_id++;
+            var list_1, list_2, list_3, list_4;
+
+            list_1 = $("<ol class = 'questset' id = list1_" + ids_html.qs_id + "></ol>");
+            list_2 = $("<ol class = 'quest' id = list2_" + ids_html.qs_id + "></ol>");
+            list_3 = $("<ol class = 'objectivelist' id = list3_" + ids_html.qs_id + "></ol>");
+            list_4 = $("<ol class = 'objective' id = list4_" + ids_html.qs_id + "></ol>");
+
+            list_item = "<li id = 'qs_'" + ids_html.qs_id + "></li>";
+
+            list_3.append(list_4);
+            list_2.append(list_3);
+            list_1.append(list_2);
+
+            list_1.prepend(list_item);
+
+            $(".questSets").append(list_1);
+
+            data.html_id = "qs_" + ids_html.qs_id;
+            break;
+
+
+
+        default:
+            break;
+    }
+}
+
 
 function addDOMElements(params) {
-var data;
+    var data;
+    var heading;
 
-data ='<li>QuestSet Id<input type="text" name="qs" id="qsId" readonly></li>';
-$(".questset").prepend(data);
+    // data = '<li>QuestSet Id<input type="text" name="qs" id="qsId" readonly></li>';
+    // heading = "<h3>test</h3>";
+    // $(".questset").prepend(data).prepend(heading);
 
-data='<li>Title <input type="text" name="" id="qTitle" ></li>';
-$(".quest").prepend(data);
+    // data = '<li>Title <input type="text" name="" id="qTitle" ></li>';
+    // $(".quest").prepend(data).prepend(heading);
 
-data='<li>ObjectiveListId<select name="" id="olList" ><option value="">Select Quest</option> </select></li>';
-$(".objectivelist").prepend(data);
+    // data = '<li>ObjectiveListId<select name="" id="olList" ><option value="">Select Quest</option> </select></li>';
+    // $(".objectivelist").prepend(data).prepend(heading);
 
-data='<li>Select Objective<select name="" id="oList" ><option value="">Select Quest</option> </select></li>';
-$(".objective").prepend(data);
+    // data = '<li>Select Objective<select name="" id="oList" ><option value="">Select Quest</option> </select></li>';
+    // $(".objective").prepend(data).prepend(heading);
 
+    traverse(data2);
 
 }
 
---
+++ quest using ul tag/test_data.js
@@ -0,0 +1,49 @@
+var data1 = [{
+    "type": "questset",
+    "id": "qs_1",
+    "children": [{
+        "type": "quest",
+        "id": "q_1",
+        "children": [{
+            "type": "objectivelist",
+            "id": "ol_1",
+            "children": [{
+                "type": "objective",
+                "id": "o_1"
+            }]
+        }]
+    }]
+}];
+
+var data2 = [
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_1"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_1"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_1"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_1"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
