2d635e1b2cad64d522597527a0427cc14e43b37a
sachinsebastian12 <sachinsebastian12@gmail.com>
2019-03-08 14:54:41 +0530

    DropDown using jquery
    Also added quest response data

--- quest using ul tag/test.js
+++ quest using ul tag/test.js
@@ -1,3 +1,6 @@
+var objectivesRetrieved = [{ "desc": "desc1", "val": "val1" }, { "desc": "desc2", "val": "val2" },
+{ "desc": "desc3", "val": "val3" }, { "desc": "desc4", "val": "val4" }];
+
 var ids_html = { o_id: 0, ol_id: 0, q_id: 0, qs_id: 0 };
 
 
@@ -13,17 +16,20 @@ function setDefaultChallangeTitle() {
     document.getElementById("qTitle").value = "Challage " + (q + 1);
 }
 
+/**
+ * This function inserts data from array of key value pairs to dropdown
+ * @param {An array with key value pair} data 
+ * @param {select tag id} elemId 
+ */
+function populateDropDown(data, elemId) {
+    elemId = "#" + elemId;
 
+    $.each(data, function (index, value) {
+        $(elemId).append($('<option></option>').val( value.val).html( value.desc));
+        console.log(value);
+    });
+}
 
-
-// function populateObjectivesIds() {
-//     var oIds = {"key1":"val1","key2":"val2"};
-
-//     $.each(oIds, function(k, v) {
-//         $('#oList').append($('<option></option>').val(k).html(v));
-//     });
-//     $('#oList').last().attr("selected","selected");
-// }
 
 function traverse(data) {
     if (data !== null && data.constructor === Array) {
@@ -43,33 +49,39 @@ function createNode(data) {
 
     var list;
     var list_item;
+    var formElements;
+    var heading = "<h3>test</h3>";
 
     switch (data.type) {
         case "objective":
             ids_html.o_id++;
-            list = "#list4" + ids_html.qs_id;
-            list_item = "<li id = 'o_'" + ids_html.o_id + "></li>";
+            list = "#list4_" + ids_html.qs_id;
+            formElements = "Select objective <select name='' id='drpobjectives'></select>";
+            list_item = "<li id = 'o_" + ids_html.o_id + "'>"+formElements+"</li>";
             $(list).append(list_item);
+            $(list).prepend(heading);
 
             data.html_id = "o_" + ids_html.o_id;
             break;
-        case "objectiveList":
+        case "objectivelist":
             ids_html.ol_id++;
-            list = "#list3" + ids_html.qs_id;
-            list_item = "<li id = 'ol_'" + ids_html.ol_id + "></li>";
+            list = "#list4_" + ids_html.qs_id;
+            list_item = "<li id = 'ol_" + ids_html.ol_id + "'>test</li>";
             $(list).before(list_item);
+            $("#list3_" + ids_html.qs_id).prepend(heading);
 
             data.html_id = "ol_" + ids_html.ol_id;
             break;
         case "quest":
             ids_html.q_id++;
-            list = "#list3" + ids_html.qs_id;
-            list_item = "<li id = 'q_'" + ids_html.q_id + "></li>";
+            list = "#list3_" + ids_html.qs_id;
+            list_item = "<li id = 'q_" + ids_html.q_id + "'>test</li>";
             $(list).before(list_item);
+            $("#list2_" + ids_html.qs_id).prepend(heading);
 
             data.html_id = "q_" + ids_html.q_id;
             break;
-        case "questSet":
+        case "questset":
             ids_html.qs_id++;
             var list_1, list_2, list_3, list_4;
 
@@ -78,13 +90,14 @@ function createNode(data) {
             list_3 = $("<ol class = 'objectivelist' id = list3_" + ids_html.qs_id + "></ol>");
             list_4 = $("<ol class = 'objective' id = list4_" + ids_html.qs_id + "></ol>");
 
-            list_item = "<li id = 'qs_'" + ids_html.qs_id + "></li>";
+            list_item = "<li id = 'qs_" + ids_html.qs_id + "'>test</li>";
 
             list_3.append(list_4);
             list_2.append(list_3);
             list_1.append(list_2);
 
             list_1.prepend(list_item);
+            list_1.prepend(heading);
 
             $(".questSets").append(list_1);
 
@@ -100,24 +113,9 @@ function createNode(data) {
 
 
 function addDOMElements(params) {
-    var data;
-    var heading;
-
-    // data = '<li>QuestSet Id<input type="text" name="qs" id="qsId" readonly></li>';
-    // heading = "<h3>test</h3>";
-    // $(".questset").prepend(data).prepend(heading);
-
-    // data = '<li>Title <input type="text" name="" id="qTitle" ></li>';
-    // $(".quest").prepend(data).prepend(heading);
-
-    // data = '<li>ObjectiveListId<select name="" id="olList" ><option value="">Select Quest</option> </select></li>';
-    // $(".objectivelist").prepend(data).prepend(heading);
-
-    // data = '<li>Select Objective<select name="" id="oList" ><option value="">Select Quest</option> </select></li>';
-    // $(".objective").prepend(data).prepend(heading);
 
     traverse(data2);
-
+    populateDropDown(objectivesRetrieved, "drpobjectives");
 }
 
 $(document).ready(function () {
